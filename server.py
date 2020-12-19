56390184a8b4e75fde058348ebb01e37873bb703
sjhangiani12 <sjhangiani12@users.noreply.github.com>
2020-12-18 19:13:58 -0800

    one more procfile

--- server.py
+++ server.py
@@ -1,16 +1,9 @@
 from flask import Flask, jsonify, request
-from flask_limiter import Limiter
-from flask_limiter.util import get_remote_address
 from waitress import serve
 from error import InvalidUsage
 from pre_trained_dt import LDAModel
 
 app = Flask(__name__)
-limiter = Limiter(
-    app,
-    key_func=get_remote_address,
-    default_limits=["200 per day", "50 per hour"]
-)
 
 def has_args(iterable, args):
     """Verify that all args are in the iterable."""
@@ -22,7 +15,6 @@ def has_args(iterable, args):
 
 
 @app.route('/', methods=['GET'])
-@limiter.exempt
 def ping():
     return 'Running'
 
@@ -34,7 +26,7 @@ def handle_invalid_usage(error):
     return response
 
 
-@app.route('/getLDAPreds', methods=['GET'])
+@app.route('/getLDAPreds', methods=['POST'])
 def get_information():
     print(request.json)
     if not has_args(request.json, ['file_url']):
